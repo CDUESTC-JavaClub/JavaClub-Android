bc568d7aae5b9b6a4efc95589709ff90e01bd1d2
Ketuer <404213506@qq.com>
2021-08-28 15:27:12 +0800

    处理Fragment复用问题，首页新增加载进度条

--- app/src/main/java/club/cduestc/MainActivity.kt
+++ app/src/main/java/club/cduestc/MainActivity.kt
@@ -2,15 +2,20 @@ package club.cduestc
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraphNavigator
+import androidx.navigation.NavigatorProvider
+import androidx.navigation.fragment.NavHostFragment
 import club.cduestc.databinding.ActivityMainBinding
+import club.cduestc.ui.dashboard.BaiFragment
+import club.cduestc.ui.home.HomeFragment
+import club.cduestc.ui.kc.KcFragment
+import club.cduestc.ui.nav.FixFragmentNavigator
+import club.cduestc.ui.notifications.NotificationsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,13 +27,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //自定义导航栏复用Fragment处理
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        navView.setupWithNavController(navController)
-
+        val fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+        val navController = NavHostFragment.findNavController(fragment!!)
+        val fragmentNavigator = FixFragmentNavigator(this, fragment.childFragmentManager, fragment.id)
+        val provider = navController.navigatorProvider
+        provider.addNavigator(fragmentNavigator)
+        val navGraph = initNavGraph(provider, fragmentNavigator)
+        navController.graph = navGraph
+        navView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            navController.navigate(item.itemId)
+            true
+        }
         window.navigationBarColor = Color.TRANSPARENT
+    }
+
+    /**
+     * 返回时，直接结束
+     */
+    override fun onBackPressed() {
+        finish()
+    }
+
+    /**
+     * 自定义导航图
+     */
+    private fun initNavGraph(provider: NavigatorProvider, fragmentNavigator: FixFragmentNavigator): NavGraph {
+        val navGraph = NavGraph(NavGraphNavigator(provider))
+
+        val destination1 = fragmentNavigator.createDestination()
+        destination1.id = R.id.navigation_home
+        destination1.className = HomeFragment::class.java.canonicalName ?: ""
+        destination1.label = resources.getString(R.string.title_home)
+        navGraph.addDestination(destination1)
+
+        val destination2 = fragmentNavigator.createDestination()
+        destination2.id = R.id.navigation_kc
+        destination2.className = KcFragment::class.java.canonicalName ?: ""
+        destination2.label = resources.getString(R.string.title_kc)
+        navGraph.addDestination(destination2)
+
+        val destination3 = fragmentNavigator.createDestination()
+        destination3.id = R.id.navigation_dashboard
+        destination3.className = BaiFragment::class.java.canonicalName ?: ""
+        destination3.label = resources.getString(R.string.title_dashboard)
+        navGraph.addDestination(destination3)
+
+        val destination4 = fragmentNavigator.createDestination()
+        destination4.id = R.id.navigation_notifications
+        destination4.className = NotificationsFragment::class.java.canonicalName ?: ""
+        destination4.label = resources.getString(R.string.title_notifications)
+        navGraph.addDestination(destination4)
+
+        navGraph.startDestination = R.id.navigation_home
+        return navGraph
     }
 }
--- app/src/main/java/club/cduestc/ui/home/HomeFragment.kt
+++ app/src/main/java/club/cduestc/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package club.cduestc.ui.home
 
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,37 +10,43 @@ import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import club.cduestc.R
 import club.cduestc.databinding.FragmentHomeBinding
+import android.widget.RelativeLayout
+
+import android.widget.ProgressBar
+import android.widget.Toast
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         val web = binding.forumWebView
+        web.visibility = View.GONE
         web.settings.javaScriptEnabled = true
-        web.settings.cacheMode = WebSettings.LOAD_NO_CACHE;
+        web.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
 
-        web.webViewClient = WebViewClient()
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.webLoad.visibility = View.GONE
+                web.visibility = View.VISIBLE
+            }
+        }
+
         web.loadUrl("https://study.cduestc.club/index.php")
-
         return binding.root
     }
 
