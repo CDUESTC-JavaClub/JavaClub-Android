af827d4ba90bd9d9b0b47af3eec7e73d62ea75c4
Ketuer <404213506@qq.com>
2021-09-06 22:03:51 +0800

    添加教务信息页面，更新README

---
+++ app/src/main/java/club/cduestc/ui/kc/sub/KcStudentActivity.kt
@@ -0,0 +1,12 @@
+package club.cduestc.ui.kc.sub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import club.cduestc.R
+
+class KcStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kc_student)
+    }
+}
