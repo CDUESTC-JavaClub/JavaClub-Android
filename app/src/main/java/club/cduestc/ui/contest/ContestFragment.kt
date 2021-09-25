1ad51cf94d0724080eade2d5782404f151f6242e
Ketuer <404213506@qq.com>
2021-09-25 21:58:15 +0800

    * 优化竞赛页面，添加新的icon

--- app/src/main/java/club/cduestc/ui/contest/ContestFragment.kt
+++ app/src/main/java/club/cduestc/ui/contest/ContestFragment.kt
@@ -11,19 +11,20 @@ import android.view.ViewGroup
 import android.webkit.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import club.cduestc.databinding.FragmentContestBinding
 import club.cduestc.databinding.FragmentHomeBinding
 import club.cduestc.util.UserManager
 
 class ContestFragment : Fragment() {
 
     private lateinit var contestViewModel: ContestViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentContestBinding? = null
     private val binding get() = _binding!!
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         contestViewModel = ViewModelProvider(this).get(ContestViewModel::class.java)
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentContestBinding.inflate(inflater, container, false)
 
 
 
