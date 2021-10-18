7bd08ac4dd45939ac436d384b806fff6a91e0048
Ketuer <404213506@qq.com>
2021-10-18 19:46:33 +0800

    更新版本信息

--- app/src/main/java/club/cduestc/util/UpdateUtil.kt
+++ app/src/main/java/club/cduestc/util/UpdateUtil.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 object UpdateUtil {
 
-    private const val version = "1.1.1"
+    private const val version = "1.1.5"
 
     fun getVersion() : String{
         return version
